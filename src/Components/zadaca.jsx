69e3309610db4beaea088ea92fb9dbb67d00dda6
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-11-02 18:53:15 +0100

    komponente1

---
+++ src/Components/zadaca.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Zadaca = () => {
+  const user = {
+    name: "Marko",
+    surname: "Horvat",
+  };
+  const formatUser = (user) => {
+    return `${user.name} ${user.surname}`;
+  };
+  return <p>Hello {formatUser(user)}</p>;
+};
+
+export default Zadaca;
